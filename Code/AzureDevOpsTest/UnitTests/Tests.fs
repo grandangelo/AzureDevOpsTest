e21997759f16c0188fd84a97e16ab717283ad7fd
grandangelo <grandangelo@zoho.com>
2022-02-04 16:35:12 +0100

    Add failing test

--- Code/AzureDevOpsTest/UnitTests/Tests.fs
+++ Code/AzureDevOpsTest/UnitTests/Tests.fs
@@ -9,3 +9,7 @@ type TestClass () =
     [<TestMethod>]
     member this.TestMethodPassing () =
         Assert.IsTrue(true);
+
+    [<TestMethod>]
+    member this.TestMethodFailing () =
+        Assert.IsTrue(false);
